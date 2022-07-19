051de2a08e9f9afcf04302be78f27c47e6d102a1
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-07-18 17:05:29 -0700

    Add(Server.js): summonerName api created

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const PORT = 8000;
+require("dotenv").config();
+const API_KEY = process.env.API_KEY;
 
 app.use(cors());
 app.use(express.json());
@@ -10,12 +12,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/api", async (req, res) => {
   try {
-    const result = await axios.get("https://fakestoreapi.com/products");
-    return res.json(result.data);
+    const summonerName = req.body.summonerID;
+    console.log("Chicken Satay", summonerName)
+    const result = await axios.get(`https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${summonerName}?api_key=${API_KEY}`);
+    return res.json(result.data)
   } catch (error) {
     console.log(error);
   }
-});
+})
+
+
+
+// app.get("/api", async (req, res) => {
+//   try {
+//     const result = await axios.get("https://fakestoreapi.com/products");
+//     return res.json(result.data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // app.get("/query", (req, res) => {
 //   req.query.title = "PM Frank";
