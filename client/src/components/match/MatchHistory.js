d49e19606aa7ad494b2f430356754ea98e39fa6b
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-07-22 18:44:04 -0700

    Add(MatchHistory): double foreloop made

--- client/src/components/match/MatchHistory.js
+++ client/src/components/match/MatchHistory.js
@@ -2,7 +2,20 @@ import React from "react";
 import { unix_timestamp } from "../../utils/utils";
 
 const MatchHistory = ({ data }) => {
-  console.log(data.puuid);
+  let party = [];
+  data.matchHistory.forEach((element) => {
+    const participants = element.info.participants;
+    participants.forEach((element2) => {
+      const player = element2.puuid;
+      if (player === data.puuid) {
+        party.push(element2);
+      } else {
+        return;
+      }
+    });
+  });
+
+  console.log(party);
 
   return (
     <div>
@@ -12,6 +25,9 @@ const MatchHistory = ({ data }) => {
             Date: {unix_timestamp(placement.info.game_datetime)}
             Game Length: {Math.round(placement.info.game_length / 60, 2)} min
           </div>
+        ))}
+        {party.map((obj, i) => (
+          <div key={i}>{obj.placement}</div>
         ))}
       </div>
     </div>
