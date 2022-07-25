8b32b5e8ec34106468df301a23ae57a48f5f1170
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-07-24 17:32:01 -0700

    Add(Home): boarder + centering added on wrapper

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./home.css";
 import SearchBar from "../components/search/Search";
 import ProfileCard from "../components/profile/ProfileCard";
 import MatchHistory from "../components/match/MatchHistory";
@@ -8,7 +9,7 @@ const Home = () => {
   const [card, setCard] = useState(false);
 
   return (
-    <div>
+    <div className="body-wrapper">
       <SearchBar setData={setData} setCard={setCard} />
       {card === true ? <ProfileCard data={data} /> : null}
       {card === true ? <MatchHistory data={data} /> : null}
